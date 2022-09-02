0a56f434a1537cff2e2f5af16acc64eeba2712b6
JL <annablue11037@gmail.com>
2022-09-01 18:01:47 -0700

    new changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -28,3 +30,15 @@ function reverseOrder(){
   var element = document.getElementById("approach-image-container");
   element.classList.add("order-first");
 }
+
+timeline.to(".test-text",5,{x:400});
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+AOS.init({
+  offset: 400, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000 // values from 0 to 3000, with step 50ms
+});
